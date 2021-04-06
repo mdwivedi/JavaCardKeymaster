43ae1785ac30350519620ee9781bcd5493a5d24f
AvinashHedage <avinash.hedage@quest-global.com>
2021-04-06 17:11:52 +0530

    Initial memory optimisation in operation state table

--- Applet/src/com/android/javacard/keymaster/KMRepository.java
+++ Applet/src/com/android/javacard/keymaster/KMRepository.java
@@ -101,15 +101,27 @@ public class KMRepository implements KMUpgradable {
     heap = JCSystem.makeTransientByteArray(HEAP_SIZE, JCSystem.CLEAR_ON_RESET);
     heapIndex = 0;
     reclaimIndex = HEAP_SIZE;
-    operationStateTable = new Object[MAX_OPS];
+    operationStateTable = JCSystem.makeTransientObjectArray(MAX_OPS, JCSystem.CLEAR_ON_RESET);
     // create and initialize operation state table.
     //First byte in the operation handle buffer denotes whether the operation is
     //reserved or unreserved.
     byte index = 0;
+    Object[] operationStateObj;
+    Object[] tempObj1;
+    Object[] tempObj2;
+
     while(index < MAX_OPS){
-      operationStateTable[index] = new Object[]{new byte[OPERATION_HANDLE_ENTRY_SIZE],
-        new Object[] {new byte[KMOperationState.MAX_DATA],
-        new Object[KMOperationState.MAX_REFS]}};
+      operationStateObj = JCSystem.makeTransientObjectArray((short) 2, JCSystem.CLEAR_ON_RESET);
+      operationStateObj[0] = JCSystem.makeTransientByteArray(OPERATION_HANDLE_ENTRY_SIZE, JCSystem.CLEAR_ON_RESET);
+
+      tempObj1 = JCSystem.makeTransientObjectArray((short) 2, JCSystem.CLEAR_ON_RESET);
+      tempObj1[0] = JCSystem.makeTransientByteArray(KMOperationState.MAX_DATA, JCSystem.CLEAR_ON_RESET);
+
+      tempObj2 = JCSystem.makeTransientObjectArray(KMOperationState.MAX_REFS, JCSystem.CLEAR_ON_RESET);
+      tempObj1[1] = tempObj2;
+
+      operationStateObj[1] = tempObj1;
+      operationStateTable[index] = operationStateObj;
       index++;
     }
     //Initialize the device locked status
@@ -191,12 +203,12 @@ public class KMRepository implements KMUpgradable {
                       KMByteBlob.cast(buf).getStartOff(),
                       KMByteBlob.cast(buf).length()))) {
         Object[] slot = (Object[]) ((Object[]) operationStateTable[index])[1];
-        JCSystem.beginTransaction();
+
         Util.arrayCopy(data, (short) 0, (byte[]) slot[0], (short) 0,
                 (short) ((byte[]) slot[0]).length);
         Object[] ops = ((Object[]) slot[1]);
         ops[0] = op;
-        JCSystem.commitTransaction();
+
         return;
       }
       index++;
@@ -208,7 +220,7 @@ public class KMRepository implements KMUpgradable {
       opId = (byte[])((Object[])operationStateTable[index])[0];
       if(0 == opId[OPERATION_HANDLE_STATUS_OFFSET]){
       	Object[] slot = (Object[])((Object[])operationStateTable[index])[1];
-      	JCSystem.beginTransaction();
+
         opId[OPERATION_HANDLE_STATUS_OFFSET] = 1;/*reserved */
         Util.arrayCopy(
             KMByteBlob.cast(buf).getBuffer(),
@@ -219,7 +231,7 @@ public class KMRepository implements KMUpgradable {
         Util.arrayCopy(data, (short) 0, (byte[]) slot[0], (short) 0, (short) ((byte[]) slot[0]).length);
         Object[] ops = ((Object[]) slot[1]);
         ops[0] = op;
-        JCSystem.commitTransaction();
+
       	break;
       }
       index++;
